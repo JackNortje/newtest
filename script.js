ef4ed862ee6364b62d4b7349a2f1ee9a84b26b62
Jack Nortje <jacknortje24@gmail.com>
2021-08-17 16:39:44 +0200

    The complete game

---
+++ script.js
@@ -0,0 +1,43 @@
+const computerChoiceDisplay = document.getElementById('computer-choice')
+const userChoiceDisplay = document.getElementById('user-choice')
+const resultDisplay = document.getElementById('result')
+const possibleChoices = document.querySelectorAll('button')
+let userChoice 
+let result
+
+possibleChoices.forEach(possibleChoice => possibleChoice.addEventListener('click', (e) => {
+   userChoice = e.target.id
+   userChoiceDisplay.innerHTML = userChoice
+   generateComputerChoices()
+   getResult()
+}))
+
+function generateComputerChoices () {
+   const randomNumber = Math.floor(Math.random() * possibleChoices.length ) +1
+
+   if (randomNumber === 1) {
+      computerChoice = 'rock'
+   }
+   if (randomNumber === 2) {
+      computerChoice = 'paper'
+   } 
+   if (randomNumber === 3) {
+      computerChoice = 'scissors'
+   }
+   computerChoiceDisplay.innerHTML = computerChoice
+}
+
+function getResult () {
+   if ((computerChoice === 'rock' && userChoice === 'scissors') ||
+      (computerChoice === 'scissors' && userChoice === 'paper') ||
+      (computerChoice === 'paper' && userChoice === 'rock')) {
+      result = 'You lost !!'
+} 
+   if (computerChoice === userChoice) {
+   result = 'Its a tie !!'
+}
+   else {
+   result = 'YOU WON !!!'
+}
+resultDisplay.innerHTML = result
+};
